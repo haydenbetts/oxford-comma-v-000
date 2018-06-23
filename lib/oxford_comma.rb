03484c2a566b834158e5fe70c324197598d9f4d3
Hayden Betts <haydenbetts@gmail.com>
2018-06-23 17:58:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-if array.length <= 1
-  return array[0]
-elsif array.length == 2
-  return "#{array[0]} and #{array[1]}"
-else
-  # refactor
-  array[-1] = "and #{array[-1]}"
-  array.join(",")
+  if array.length <= 1
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    # refactor
+    array[-1] = "and #{array[-1]}"
+    array.join(",")
+  end
 end
